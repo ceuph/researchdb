b6453f23161f966e70db252559a71836bb5b8779
Joey O. Chua <joeyochua@gmail.com>
2019-07-22 18:31:22 +0800

    complete app:import-data command

--- src/Command/ImportDataCommand.php
+++ src/Command/ImportDataCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,10 +13,19 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 use App\Entity\Document;
 use App\Entity\DocumentAttachment;
 use App\Entity\DocumentAuthor;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ImportDataCommand extends Command
 {
     protected static $defaultName = 'app:import-data';
+    private $container;
+    private $entityManager;
+
+    public function __construct(?string $name = null, EntityManagerInterface $entityManager)
+    {
+        parent::__construct($name);
+        $this->entityManager = $entityManager;
+    }
 
     protected function configure()
     {
@@ -25,16 +35,17 @@ class ImportDataCommand extends Command
             '"<firstname> <mi>. <lastname>" format and delimeted by semicolon ";". Filename should not include file extension. ' .
             'File should be in PDF format."')
             ->addArgument('excel_path', InputArgument::REQUIRED, 'Excel file to read')
-            ->addArgument('directory', InputArgument::OPTIONAL, 'Directory containing PDF files')
+            ->addArgument('source_directory', InputArgument::OPTIONAL, 'Directory containing PDF files')
+            ->addArgument('destination_directory', InputArgument::OPTIONAL, 'Public directory for PDF files')
             ->addOption('skip', 's', InputOption::VALUE_OPTIONAL, 'Number of rows to skip', 0);
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /* @var $em \Doctrine\ORM\EntityManagerInterface */
         $io = new SymfonyStyle($input, $output);
         $spreadsheet = IOFactory::load($input->getArgument('excel_path'));
-
         $x = $input->getOption('skip') + 1;
 
         while (true) {
@@ -44,14 +55,60 @@ class ImportDataCommand extends Command
             $month = $spreadsheet->getActiveSheet()->getCell('D' . $x)->getValue();
             $day = $spreadsheet->getActiveSheet()->getCell('E' . $x)->getValue();
             $author = $spreadsheet->getActiveSheet()->getCell('F' . $x)->getValue();
-            $file = $spreadsheet->getActiveSheet()->getCell('G' . $x)->getValue();
-            $remarks = $spreadsheet->getActiveSheet()->getCell('H' . $x)->getValue();
+            $remarks = $spreadsheet->getActiveSheet()->getCell('G' . $x)->getValue();
+            $abstract = $spreadsheet->getActiveSheet()->getCell('H' . $x)->getValue();
+            $fulltext = $spreadsheet->getActiveSheet()->getCell('I' . $x)->getValue();
 
-            $io->text($subject);
-            $x++;
             if (strlen(trim($subject)) == 0) {
                 break;
             }
+
+            $document = new Document();
+            $document->setSubject($subject);
+            $document->setBody($body);
+            $document->setYearCreated($year);
+            $document->setMonthCreated(strlen(trim($month)) > 0 ?$month : null);
+            $document->setDayCreated(strlen(trim($day)) > 0 ? $day : null);
+            $document->setRemarks(strlen(trim($remarks)) > 0 ? $remarks : null);
+            $this->addAttachment(
+                $document,
+                $input->getArgument('source_directory') . DIRECTORY_SEPARATOR . $abstract . '.pdf',
+                DocumentAttachment::TYPE_ABSTRACT,
+                $input->getArgument('destination_directory')
+            );
+            $this->addAttachment(
+                $document,
+                $input->getArgument('source_directory') . DIRECTORY_SEPARATOR . $fulltext . '.pdf',
+                DocumentAttachment::TYPE_FULLTEXT,
+                $input->getArgument('destination_directory')
+            );
+
+            if (strlen(trim($author)) > 0) {
+                $authors = explode(';', $author);
+                foreach ($authors as $author_value) {
+                    $documentAuthor = new DocumentAuthor($author_value);
+                    $documentAuthor->setDisplayName($author_value);
+                    $document->addDocumentAuthor($documentAuthor);
+                    $this->entityManager->persist($documentAuthor);
+                }
+            }
+            $this->entityManager->persist($document);
+            $x++;
+        }
+        $this->entityManager->flush();
+    }
+
+    private function addAttachment(Document $document, $sourceFile, $type, $destinationDirectory)
+    {
+        if (file_exists($sourceFile)) {
+            $destination_file_name = uniqid() . '.pdf';
+            $destination_file = $destinationDirectory . DIRECTORY_SEPARATOR . $destination_file_name;
+            copy($sourceFile, $destination_file);
+            $documentAttachment = new DocumentAttachment();
+            $documentAttachment->setPath($destination_file_name);
+            $documentAttachment->setType($type);
+            $document->addDocumentAttachment($documentAttachment);
+            $this->entityManager->persist($documentAttachment);
         }
     }
 }
